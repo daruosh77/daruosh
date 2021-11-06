4859298468f9a9ea58b18aa71bfd41bada9a6206
Solomun36 <Paramount.pro27@gmail.com>
2021-11-06 22:01:02 +0330

    complite toggle button

--- Dashboard/app.js
+++ Dashboard/app.js
@@ -1,22 +1,34 @@
 
 // ************* Variables ****************
 
-// add hovered class in selector list item
+//? add hovered class in selector list item
 let list = document.querySelectorAll('.nav li');
+
+//? active toggle button & navigation & main classes 
+let toggle = document.querySelector('.toggle')
+let nav = document.querySelector('.nav')
+let main = document.querySelector('.main')
 
 // ************** Codes *******************
 
-function activeLinks()
-{
+function activeLinks(){
+
     list.forEach((item)=>
     item.classList.remove('hovered'));
     this.classList.add('hovered')
+
 }
 
 list.forEach((item) =>
 item.addEventListener('mouseover',activeLinks));
 
 
+toggle.onclick = function(){
 
+    toggle.classList.toggle('active')
+    nav.classList.toggle('active')
+    main.classList.toggle('active')
+
+}
 
 
